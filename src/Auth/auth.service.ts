5c9a6e6d95bdcf291a1357e3802b5eb9a5746c19
NattawatC <64011478@kmitl.ac.th>
2024-04-06 02:15:00 +0700

    Change the auth logic

--- src/Auth/auth.service.ts
+++ src/Auth/auth.service.ts
@@ -1,38 +1,29 @@
-// auth.service.ts
-import { Injectable } from '@nestjs/common';
-import { PatientService } from 'src/patient/patient.service';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { compare } from 'bcrypt';
 import { UserLoginDto } from 'src/patient/dtos/UserLogin.dto';
 import { Patient } from 'src/typeorm/entities/Patient';
+import { PatientService } from 'src/patient/patient.service'; // Import the PatientService
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly patientService: PatientService,
-    // private readonly staffService: StaffService,
+    private patientService: PatientService,
   ) {}
 
-  async authenticatePatient(userLoginDto: UserLoginDto): Promise<Patient | null> {
-    // Retrieve patient from the database based on the Thai ID
-    const patient = await this.patientService.findByThaiId(userLoginDto.username);
+  async authenticatePatient(userLoginDto: UserLoginDto): Promise<Patient> {
+    const { username, password } = userLoginDto;
 
-    // Check if the patient exists and if the password matches
-    // if (patient) {
-    //   const isAuthenticated = await compare(userLoginDto.password, patient.password); // Compare hashed passwords
-    //   return { isAuthenticated };
-    // }
+    // Find the patient by Thai ID using the PatientService
+    const patient = await this.patientService.findByThaiId(username);
+
+    // Check password using bcrypt
+    const isValidPassword = await compare(password, patient.password);
+
+    if (!isValidPassword) {
+      throw new UnauthorizedException('Wrong password');
+    }
+
     return patient;
   }
-
-//   async authenticateStaff(fullname: string, password: string): Promise<{ isAuthenticated: boolean }> {
-//     // Retrieve staff member from the database based on the full name
-//     const staff = await this.staffService.findByFullname(fullname);
-
-//     // Check if the staff member exists and if the password matches
-//     if (staff) {
-//       const isAuthenticated = await compare(password, staff.password); // Compare hashed passwords
-//       return { isAuthenticated };
-//     }
-//     return { isAuthenticated: false };
-//   }
 }
+
