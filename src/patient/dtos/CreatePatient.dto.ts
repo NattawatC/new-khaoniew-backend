f36e8195b82b4c3c64151fbf4ae3d868fbb2c2fa
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2024-04-07 02:58:42 +0700

    made health risk score column nullable

--- src/patient/dtos/CreatePatient.dto.ts
+++ src/patient/dtos/CreatePatient.dto.ts
@@ -1,11 +1,37 @@
+import { IsString, IsInt, IsNotEmpty, IsOptional } from 'class-validator';
+
 export class CreatePatientDto {
+  @IsNotEmpty()
+  @IsString()
   thaiId: string;
+
+  @IsNotEmpty()
+  @IsString()
   firstname: string;
+
+  @IsNotEmpty()
+  @IsString()
   lastname: string;
+
+  @IsNotEmpty()
+  @IsInt()
   age: number;
+
+  @IsNotEmpty()
+  @IsString()
   gender: string;
+
+  @IsNotEmpty()
+  @IsString()
   address: string;
+
+  @IsNotEmpty()
+  @IsString()
   phoneNumber: string;
+
   medicalCondition?: string[];
-  healthRiskScore: number;
+
+  @IsOptional()
+  @IsInt()
+  healthRiskScore: number | null = null;
 }
