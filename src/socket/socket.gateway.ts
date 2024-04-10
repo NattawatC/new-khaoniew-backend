17ed872324b72d6b4f9355aa084ca1f5c38dc783
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2024-04-10 12:26:46 +0700

    .

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -1,11 +1,26 @@
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
-export class SocketGateway {
+export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+
     @WebSocketServer() server: Server;
 
-    handleConnection(client: any, ...args: any[]) {
-        console.log('Client connected');
+    afterInit(server: Server) {
+        console.log('Socket server initialized');
+    }
+
+    handleConnection(client: Socket, ...args: any[]) {
+        console.log(`Client ${client.id} connected`);
+    }
+
+    handleDisconnect(client: Socket) {
+        console.log(`Client ${client.id} disconnected`);
+    }
+
+    @SubscribeMessage('binaryData') // Listen for the 'binaryData' event
+    handleBinaryData(client: Socket, data: Uint8Array) {
+        console.log(`Binary data received from client ${client.id}:`, data);
+        return 'Binary data received successfully'; // Acknowledge the message
     }
 }
