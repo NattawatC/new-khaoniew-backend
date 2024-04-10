fef9817a39825220a39bca6827a5dbbbd70142b3
NattawatC <64011478@kmitl.ac.th>
2024-04-10 18:55:49 +0700

    Update staff table

--- src/typeorm/entities/Staff.ts
+++ src/typeorm/entities/Staff.ts
@@ -1,19 +1,22 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-import * as bcrypt from "bcrypt";
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Entity()
 export class Staff {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    fullName: string;
+  @Column()
+  fullName: string;
 
-    @Column()
-    password: string;
+  @Column()
+  password: string;
 
-    @BeforeInsert()
-    async hashPassword() {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+
+  @Column({ default: '' })
+  address: string;
 }
